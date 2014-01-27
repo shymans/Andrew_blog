4525336b6c8075f355ebefbec98a44691d104029
Austin Shymanski <austin.shymanski@gmail.com>
2014-01-27 13:19:18 -0500

    added nav bar and started on new article page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Blogger::Application.routes.draw do
   get "articles/show"
   get "articles/new"
   get "articles/edit"
+
+  root 'articles#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
