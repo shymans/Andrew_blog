78de9481a640caca3357bcd35ce4aafa6d66046e
Austin Shymanski <austin.shymanski@gmail.com>
2014-01-31 09:28:05 -0500

    fixed views, first draft copy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -46,6 +46,6 @@ class ArticlesController < ApplicationController
 
   private
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :url, :body)
   end
 end
